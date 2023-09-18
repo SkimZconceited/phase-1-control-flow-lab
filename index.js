4c00da902528c68e2e230545c9372140f41f3068
Stanley 'SkimZ' Kimambo <Stanleykimambo@gmail.com>
2023-09-18 17:11:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+  if (x <= 400) {
+    return 'This one is on me!'
+  } else if (x > 2000 & x < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(y){
   // Write your code here!
+  return y === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(t){
   // Write your code here!
+  switch (t) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+
+    default:
+      return 'Bye.'
+      break;
+  }
 }
